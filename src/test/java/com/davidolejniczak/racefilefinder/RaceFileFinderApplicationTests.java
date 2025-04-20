4b8a546bfd7a30dcfeb38d64d966117a0e6cb976
davidolejniczak <David.olejniczak@icloud.com>
2025-04-20 13:30:29 -0400

    Loaded up SpringBoot Project

---
+++ src/test/java/com/davidolejniczak/racefilefinder/RaceFileFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidolejniczak.racefilefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaceFileFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
