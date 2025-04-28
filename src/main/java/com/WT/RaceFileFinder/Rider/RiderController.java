24b69c83a8b352cdb494fd7557560a4600bb953d
davidolejniczak <David.olejniczak@icloud.com>
2025-04-28 19:10:33 -0400

    More trying

--- src/main/java/com/WT/RaceFileFinder/Rider/RiderController.java
+++ src/main/java/com/WT/RaceFileFinder/Rider/RiderController.java
@@ -11,9 +11,13 @@ import java.util.List;
 @RequestMapping(path = "api/rider")
 public class RiderController {
 
+    private final RiderService riderService;
+
+    @GetMapping
     public List<Rider> getRiders(
-        @RequestParam(required = false) String riderName,
-        @RequestParam(required = false) String nationality, 
-        @RequestParam(required = false) String team)
+            @RequestParam(required = false) String riderName,
+            @RequestParam(required = false) String nationality,
+            @RequestParam(required = false) String team) {
+    }
 
 }
