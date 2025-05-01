e623888777d74de67450d28f2b606d8a2cd71039
davidolejniczak <David.olejniczak@icloud.com>
2025-05-01 18:48:54 -0400

    Added Service layer to code

--- Backend/src/main/java/com/WT/RaceFileFinder/Rider/RiderService.java
+++ Backend/src/main/java/com/WT/RaceFileFinder/Rider/RiderService.java
@@ -9,4 +9,48 @@ public class RiderService {
 
     private final RiderRepository riderRepository;
 
+    public RiderService(RiderRepository riderRepository) {
+        this.riderRepository = riderRepository;
+    }
+
+    public List<Rider> getAllRiders(Sort sort) {
+        return riderRepository.findAllRiders(sort);
+    }
+
+    public Rider getRiderByName(String riderName) {
+        Optional<Rider> foundRider = riderRepository.findByRiderName(riderName);
+        if (foundRider.isPresent())
+            return foundRider.get();
+
+        foundRider = riderRepository.findByRiderNameIgnoreCase(riderName);
+        if (foundRider.isPresent())
+            return foundRider.get();
+
+        foundRider = riderRepository.searchByName(riderName);
+        if (foundRider.isPresent())
+            return foundRider.get();
+
+        throw new RiderNotFoundException("Rider not found");
+    }
+
+    public Rider getByTeam(String team) {
+        return riderRepository.findByTeam(team);
+    }
+
+    public List<Rider> getByNameContaining(String keyword) {
+        return riderRepository.findByNameContaining(keyword);
+    }
+
+    public List<Rider> getByNationality(String nationality) {
+        return riderRepository.findByNationality(nationality);
+    }
+
+    public List<Rider> getByTeamAndNationality(String team, String nationality) {
+        return riderRepository.findByTeamAndNationality(team, nationality);
+    }
+
+    public Rider saveRider(String riderName, String team, String nationality) {
+        return riderRepository.saveRider(riderName, team, nationality);
+    }
+
 }
