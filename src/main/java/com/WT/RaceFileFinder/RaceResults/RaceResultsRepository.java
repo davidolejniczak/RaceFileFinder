41728ad3bae259e7a395e995f5350584d6fd429f
davidolejniczak <David.olejniczak@icloud.com>
2025-06-01 15:47:03 -0400

    query 2

--- src/main/java/com/WT/RaceFileFinder/RaceResults/RaceResultsRepository.java
+++ src/main/java/com/WT/RaceFileFinder/RaceResults/RaceResultsRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface RaceResultsRepository
         extends JpaRepository<RaceResults, RaceResultsId>, PagingAndSortingRepository<RaceResults, RaceResultsId> {
 
-    @Query(value = """SELECT * FROM raceresults WHERE unaccent(racename) ILIKE unaccent(:raceName) ORDER BY CASE WHEN position ~ '^[0-9]+$' THEN CAST(position AS INTEGER) ELSE NULL END ASC NULLS LAST, position ASC""",nativeQuery = true)
+    @Query(value = """SELECT * FROM raceresults WHERE unaccent(racename) ILIKE unaccent(:raceName) ORDER BY CASE WHEN position ~ '^[0-9]+$' THEN 0 ELSE 1 END ASC, CASE WHEN position ~ '^[0-9]+$' THEN CAST(position AS INTEGER) ELSE NULL END ASC, position ASC""",nativeQuery = true)
     List<RaceResults> findByRaceName(String raceName);
 
 }
