61d7d89d32f760caa5236f1455298b40c84c98bb
davidolejniczak <David.olejniczak@icloud.com>
2025-06-07 10:44:47 -0400

    Logging

--- src/main/java/com/WT/RaceFileFinder/RaceResults/RaceResultsController.java
+++ src/main/java/com/WT/RaceFileFinder/RaceResults/RaceResultsController.java
@@ -17,7 +17,16 @@ public class RaceResultsController {
 
     @GetMapping("/r")
     public List<RaceResults> getRaceResults(@RequestParam String racename) {
+        System.out.println("CONTROLLER: ========== NEW REQUEST ==========");
+        System.out.println("CONTROLLER: Received GET request for race results");
+        System.out.println("CONTROLLER: Race name parameter: '" + racename + "'");
+        
         List<RaceResults> results = raceResultsService.getRaceResults(racename);
+        
+        System.out.println("CONTROLLER: Service returned " + results.size() + " results");
+        System.out.println("CONTROLLER: Sending response with " + results.size() + " race results");
+        System.out.println("CONTROLLER: ========== REQUEST COMPLETE ==========");
+        
         return results;
     }
 
