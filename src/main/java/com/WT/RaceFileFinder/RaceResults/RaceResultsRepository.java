66066e4c1702902551167e3ca7b8d43a63ecee48
davidolejniczak <David.olejniczak@icloud.com>
2025-06-07 10:21:40 -0400

    Table query change

--- src/main/java/com/WT/RaceFileFinder/RaceResults/RaceResultsRepository.java
+++ src/main/java/com/WT/RaceFileFinder/RaceResults/RaceResultsRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface RaceResultsRepository
         extends JpaRepository<RaceResults, RaceResultsId>, PagingAndSortingRepository<RaceResults, RaceResultsId> {
 
-    @Query(value = "SELECT * FROM raceresults WHERE unaccent(racename) ILIKE unaccent(:raceName) ORDER BY CAST(riderposition AS INTEGER) ASC", nativeQuery = true)
+    @Query(value = "SELECT raceresults.racename, raceresults.riderposition, raceresults.ridername, riders.strava FROM raceresults JOIN riders ON raceresults.ridername = riders.ridername  WHERE unaccent(raceresults.racename) ILIKE unaccent(:raceName) ORDER BY CAST(raceresults.riderposition AS INTEGER) ASC", nativeQuery = true)
     List<RaceResults> findByRaceName(String raceName);
 
 }
