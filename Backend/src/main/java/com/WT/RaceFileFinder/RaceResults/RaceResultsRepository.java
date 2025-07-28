4067c59f29466aa56a9135c8f62454b14b3976db
DavidOlejniczak <david.olejniczak@icloud.com>
2025-07-27 22:17:38 -0400

    debug 6

--- Backend/src/main/java/com/WT/RaceFileFinder/RaceResults/RaceResultsRepository.java
+++ Backend/src/main/java/com/WT/RaceFileFinder/RaceResults/RaceResultsRepository.java
@@ -7,9 +7,9 @@ import java.util.List;
 public interface RaceResultsRepository
         extends JpaRepository<RaceResults, RaceResultsId> {
 
-    @Query(value = "SELECT raceresults.racename, raceresults.riderposition, raceresults.ridername, riders.strava AS riderstrava FROM raceresults JOIN riders ON LOWER(raceresults.ridername) = LOWER(riders.ridername)  WHERE unaccent(raceresults.racename) ILIKE unaccent(:raceName) ORDER BY CAST(raceresults.riderposition AS INTEGER) ASC", nativeQuery = true)
+    @Query(value = "SELECT raceresults.racename, raceresults.riderposition, raceresults.ridername, riders.riderstravalink AS riderstrava FROM raceresults JOIN riders ON LOWER(raceresults.ridername) = LOWER(riders.ridername)  WHERE unaccent(raceresults.racename) ILIKE unaccent(:raceName) ORDER BY CAST(raceresults.riderposition AS INTEGER) ASC", nativeQuery = true)
     List<RaceResults> findByRaceName(String raceName);
 
-    @Query(value = "SELECT raceresults.racename, raceresults.riderposition, raceresults.ridername, raceresults.countryCode, raceresults.raceDate FROM raceresults JOIN riders ON LOWER(raceresults.ridername) = LOWER(riders.ridername) WHERE riders.teamId = (:teamId) ORDER BY CAST(raceresults.riderposition AS INTEGER) ASC", nativeQuery = true)
+    @Query(value = "SELECT raceresults.racename, raceresults.riderposition, raceresults.ridername, raceresults.countryCode, raceresults.raceDate FROM raceresults JOIN riders ON LOWER(raceresults.ridername) = LOWER(riders.ridername) WHERE riders.teamid = (:teamId) ORDER BY CAST(raceresults.riderposition AS INTEGER) ASC", nativeQuery = true)
     List<RaceResults> findByTeamId(String teamId);
 }
