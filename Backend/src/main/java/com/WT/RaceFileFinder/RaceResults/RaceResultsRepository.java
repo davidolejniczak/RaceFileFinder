bb70618d8487b33716539a1df76521ef5b58f380
DavidOlejniczak <david.olejniczak@icloud.com>
2025-07-27 22:49:55 -0400

    debug 8

--- Backend/src/main/java/com/WT/RaceFileFinder/RaceResults/RaceResultsRepository.java
+++ Backend/src/main/java/com/WT/RaceFileFinder/RaceResults/RaceResultsRepository.java
@@ -7,9 +7,12 @@ import java.util.List;
 public interface RaceResultsRepository
         extends JpaRepository<RaceResults, RaceResultsId> {
 
-    @Query(value = "SELECT raceresults.racename, raceresults.riderposition, raceresults.ridername, riders.riderstravalink AS riderstrava FROM raceresults JOIN riders ON LOWER(raceresults.ridername) = LOWER(riders.ridername)  WHERE unaccent(raceresults.racename) ILIKE unaccent(:raceName) ORDER BY CAST(raceresults.riderposition AS INTEGER) ASC", nativeQuery = true)
-    List<RaceResults> findByRaceName(String raceName);
+    @Query(value = "SELECT raceresults.racename, raceresults.riderposition, raceresults.ridername, riders.riderstravalink FROM raceresults JOIN riders ON LOWER(raceresults.ridername) = LOWER(riders.ridername)  WHERE unaccent(raceresults.racename) ILIKE unaccent(:racename) ORDER BY CAST(raceresults.riderposition AS INTEGER) ASC", nativeQuery = true)
+    List<RaceResults> findByRaceName(String racename);
 
     @Query(value = "SELECT raceresults.racename, raceresults.riderposition, raceresults.ridername, raceresults.countrycode, raceresults.racedate FROM raceresults JOIN riders ON LOWER(raceresults.ridername) = LOWER(riders.ridername) WHERE riders.teamid = (:teamId) ORDER BY CAST(raceresults.riderposition AS INTEGER) ASC", nativeQuery = true)
     List<RaceResults> findByTeamId(String teamId);
+
+    @Query(value = "SELECT raceresults.racename, raceresults.riderposition, raceresults.ridername, riders.riderstravalink FROM raceresults JOIN riders ON LOWER(raceresults.ridername) = LOWER(riders.ridername)  WHERE unaccent(raceresults.racename) ILIKE unaccent(:racename) ORDER BY CAST(raceresults.riderposition AS INTEGER) ASC", nativeQuery = true)
+    List<Object[]> findByRaceNameRaw(String racename);
 }
--- Backend/src/main/java/com/WT/RaceFileFinder/RaceResults/RaceResultsService.java
+++ Backend/src/main/java/com/WT/RaceFileFinder/RaceResults/RaceResultsService.java
@@ -2,6 +2,7 @@ package com.WT.RaceFileFinder.RaceResults;
 
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class RaceResultsService {
@@ -12,34 +13,49 @@ public class RaceResultsService {
         this.raceResultsRepository = raceResultsRepository;
     }
 
-    public List<RaceResults> getRaceResults(String racename) {
-        System.out.println("SERVICE: Processing race name: '" + racename + "'");
-        System.out.println("SERVICE: Calling repository.findByRaceName() with parameter: " + racename);
+    // public List<RaceResults> getRaceResults(String racename) {
+    // System.out.println("SERVICE: Processing race name: '" + racename + "'");
+    // System.out.println("SERVICE: Calling repository.findByRaceName() with
+    // parameter: " + racename);
 
-        List<RaceResults> results = raceResultsRepository.findByRaceName(racename);
+    // List<RaceResults> results = raceResultsRepository.findByRaceName(racename);
 
-        System.out.println("SERVICE: Repository returned " + results.size() + " results");
+    // System.out.println("SERVICE: Repository returned " + results.size() + "
+    // results");
 
-        // Print details of each result
-        if (results.size() > 0) {
-            System.out.println("SERVICE: Details of results:");
-            for (int i = 0; i < results.size(); i++) {
-                RaceResults result = results.get(i);
-                System.out.println("  Result " + (i + 1) + " - Race: '" + result.getRaceName() +
-                        "', Position: '" + result.getRiderPosition() +
-                        "', Rider: '" + result.getRiderName() +
-                        "', Strava: '" + result.getRiderStrava() + "'");
-            }
-        } else {
-            System.out.println("SERVICE: No results found for race name: " + racename);
-        }
+    // // Print details of each result
+    // if (results.size() > 0) {
+    // System.out.println("SERVICE: Details of results:");
+    // for (int i = 0; i < results.size(); i++) {
+    // RaceResults result = results.get(i);
+    // System.out.println(" Result " + (i + 1) + " - Race: '" + result.getRaceName()
+    // +
+    // "', Position: '" + result.getRiderPosition() +
+    // "', Rider: '" + result.getRiderName() +
+    // "', Strava: '" + result.getRiderStrava() + "'");
+    // }
+    // } else {
+    // System.out.println("SERVICE: No results found for race name: " + racename);
+    // }
 
-        System.out.println("SERVICE: Returning " + results.size() + " results to controller");
-        return results;
-    }
+    // System.out.println("SERVICE: Returning " + results.size() + " results to
+    // controller");
+    // return results;
+    // }
 
     public List<RaceResults> getRaceResultsByTeamId(String teamId) {
         return raceResultsRepository.findByTeamId(teamId);
+    }
+
+    public List<RaceResultsWithStravaDTO> getRaceResults(String racename) {
+        List<Object[]> rawResults = raceResultsRepository.findByRaceNameRaw(racename);
+        return rawResults.stream()
+                .map(obj -> new RaceResultsWithStravaDTO(
+                        (String) obj[0],
+                        (String) obj[1],
+                        (String) obj[2],
+                        (String) obj[3]))
+                .collect(Collectors.toList());
     }
 
 }
